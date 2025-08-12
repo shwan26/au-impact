8d4a44802c1d27ddf108ee065f6fce0197a96583
Shwan <syanshwan.26@gmail.com>
2025-08-12 16:46:09 +0700

    fix: Go to Shwan

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to Shwan -
         </a>
       </footer>
     </div>
